532a67afd575542d42fa14981ee48120b00ca628
Ayyappa Muthusami <ayyappamuthusami@Ayyappas-MacBook-Pro.local>
2020-03-26 22:50:23 -0700

    added javascript code to handleClick function and added event for onclick, subsequently refreshed table data

--- static/js/app.js
+++ static/js/app.js
@@ -13,4 +13,18 @@ function buildTable(data) {
             cell.text(val);
         });
     });
-}
+};
+
+function handleClick() {
+    //get teh datetime value from filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    //check if datetime value is entered, if so use it to filter the data
+    // whose dateTime matches the filtered date time entered
+    if (date) {
+        filteredData = filteredData.filter(row => row.dateTime === date);
+    }
+    //rebuild table usig the filtered data
+    //if no dateTime was entered the original data is retained
+    buildTable(filteredData);
+};
